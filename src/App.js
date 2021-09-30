4477916b180031b564f8e0b257ca09f8c608e62a
Jonas B. Halvorsen <jbh93hoved.annet@gmail.com>
2021-09-30 09:36:18 +0200

    delete function in 6.2 finsihed. 6.2 finished

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ import { useState } from 'react';
 const App = () => {
   
   const [todos, setTodos] = useState([{title : "Lorem Ipsum", 
-  content : "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."}, {title : "Lorem Ipsum", 
-  content : "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."}, {title : "Lorem Ipsum", 
-  content : "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."}]);
+  content : "Neque porro quisquam est qui dolorem ipsum qsuia dolor sit amet, consectetur, adipisci velit..."}, {title : "Lorem Ipsum", 
+  content : "Neque porro quisquam est qui dolorem ipssum quia dolor sit amet, consectetur, adipisci velit..."}, {title : "Lorem Ipsum", 
+  content : "Neque porro quisquam est qui dolorem ipsum quia doslor sit amet, consectetur, adipisci velit..."}]);
+
   return (
     <>
       <header>
@@ -18,7 +19,7 @@ const App = () => {
       </header>
       <main>
         <Form addTodos={setTodos}/>
-        <Todos list={todos}/>
+        <Todos list={todos} deleteTodos={setTodos}/>
       </main>
     </>
   );
